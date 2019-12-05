dd945990cc3209e71a87a43489d1227368206d4f
Brenden Riley <BRiley001@west-mec.org>
2019-12-05 15:28:04 -0700

    I mean it works
    Hey it exists, it does what I want so far, now need to build out actual functionality.

--- index.js
+++ index.js
@@ -3,26 +3,63 @@ var app = express();
 var fetch = require("node-fetch");
 var port = 5500;
 
-app.use(express.static('public'));
+//Global Variables
+var diginum =0;
+var pokenum =0;
+var yuginum =0;
+
+app.use(express.static(__dirname));
 
 app.listen(port, function () {
     console.log('Server listening on localhost:%s', port);
 });
 
-app.get("/", function (req, res) {
+app.get("/digimon", function (req, res) {
     getData("https://digimon-api.herokuapp.com/api/digimon", (data) => {
-        var limit = 1;
-        var html = '<link rel="stylesheet" href="styles.css">';
-        for (let i = 0; i < limit; i++) {
-            html += "<section class='digi'>";
-            html += `<p>${data[i].name}</p>`;
-            html += `<img src='${data[i].img}'>`;
-            html += "</section>";
-        }
+        let html = `<section class='digi'`;
+        html += `<p>${data[diginum].name}</p>`;
+        html += `<img src='${data[diginum].img}'>`;
+        html +=`</section>`
+        res.send(html);
+    })    
+});
+
+app.get("/", function (req, res){
+    let html= `<a src="index.html"></a>`;
+    res.send(html);
+})
+
+app.get("/pokemon", function (req, res) {
+    getData("https://pokeapi.co/api/v2/pokemon/1/", (data) => {
+        // console.log(data.name)
+        let html=`<section id="digi"`
+        html+=`<div>${data.name}</div>`;
+        // console.log(data.sprites.front_default)
+        html += `<img src="${data.sprites.front_default}" />`;
+        html+=`</section>`;
+
         res.send(html);
     })
-
 });
+
+app.get("/yugioh", function (req, res) {
+    getData("", (data) => {
+        res.send();
+    })    
+});
+
+
+function displayPoke(data){
+
+}
+
+function displayDigi(data){
+
+}
+
+function dispalyYugi(data){
+
+}
 
 function getData(url, callback) {
     fetch(url).then((Response) => Response.json())
---
+++ test.js
@@ -0,0 +1,13 @@
+/*
+
+Author:Brenden Riley
+Date:12.5.19
+
+*/
+let pokenum=0;
+let request = new XMLHttpRequest();
+request.open('GET', `https://pokeapi.co/api/v2/pokemon/1`)
+
+function changePokemon(){
+    
+}
